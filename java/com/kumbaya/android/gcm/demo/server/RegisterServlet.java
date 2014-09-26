069ac5d5d060f2a66df4c08466eb02a5d313d2e2
goto <samuelgoto@gmail.com>
2014-09-26 15:33:33 -0700

    Fixing appengines build

--- java/com/kumbaya/android/gcm/demo/server/RegisterServlet.java
+++ java/com/kumbaya/android/gcm/demo/server/RegisterServlet.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.google.android.gcm.demo.server;
+package com.kumbaya.android.gcm.demo.server;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
