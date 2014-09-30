6c960fef2c1e737dc3a42401e9b755815d4d49b8
goto <samuelgoto@gmail.com>
2014-09-29 22:04:16 -0700

    Adding a fragment activity UI, to more easily allow scaling the UI.

--- java/com/kumbaya/android/client/DemoActivity.java
+++ java/com/kumbaya/android/client/DemoActivity.java
@@ -58,8 +58,11 @@ import android.content.ServiceConnection;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.IBinder;
-import android.app.Fragment;
-import android.app.FragmentManager;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -79,41 +82,69 @@ import android.widget.TextView.OnEditorActionListener;
 /**
  * Main UI for the demo app.
  */
-public class DemoActivity extends Activity {
-    private final ServiceConnection connection = new ServiceConnection() {
+public class DemoActivity extends FragmentActivity {
+	private final ServiceConnection connection = new ServiceConnection() {
 		@Override
 		public void onServiceConnected(ComponentName name, IBinder b) {
-            LocalBinder binder = (LocalBinder) b;
-            service = Optional.of(binder.getService());
+			LocalBinder binder = (LocalBinder) b;
+			service = Optional.of(binder.getService());
 		}
 
 		@Override
 		public void onServiceDisconnected(ComponentName name) {
 		}
-    };
+	};
 
-    private Optional<BackgroundService> service = Optional.absent();
-    TextView mDisplay;
+	private Optional<BackgroundService> service = Optional.absent();
+	DemoCollectionPagerAdapter mDemoCollectionPagerAdapter;
+	ViewPager mViewPager;
+	private final CreateFragment createFragment = new CreateFragment();
+	private final DebugFragment debugFragment = new DebugFragment();
+	private final SearchFragment searchFragment = new SearchFragment();
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-		
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
 		Intent intent = new Intent(this, BackgroundService.class);
 		startService(intent);
-		
-		Intent i = new Intent(this, BackgroundService.class);
-        bindService(i, connection, Context.BIND_AUTO_CREATE);
-		
-		checkNotNull(SERVER_URL, "SERVER_URL");
-        checkNotNull(SENDER_ID, "SENDER_ID");
 
-        setContentView(R.layout.main);
+		Intent i = new Intent(this, BackgroundService.class);
+		bindService(i, connection, Context.BIND_AUTO_CREATE);
+
+		checkNotNull(SERVER_URL, "SERVER_URL");
+		checkNotNull(SENDER_ID, "SENDER_ID");
+
+		setContentView(R.layout.main);
+
+		registerReceiver(mHandleMessageReceiver,
+				new IntentFilter(DISPLAY_MESSAGE_ACTION));
+		
+		// ViewPager and its adapters use support library
+        // fragments, so use getSupportFragmentManager.
+		Fragment fragments[] = {
+				createFragment,
+				searchFragment, 
+				debugFragment};
+
+        mDemoCollectionPagerAdapter =
+                new DemoCollectionPagerAdapter(
+                        getSupportFragmentManager(),
+                        fragments);
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setAdapter(mDemoCollectionPagerAdapter);
+        mViewPager.setCurrentItem(1);
+        mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+            @Override
+            public void onPageSelected(int position) {
+            	if (position == 2) {
+        			debugFragment.setText(service.get().toString());
+            	}
+            }
+        });
         
-        mDisplay = (TextView) findViewById(R.id.display);
-        registerReceiver(mHandleMessageReceiver,
-                new IntentFilter(DISPLAY_MESSAGE_ACTION));
-        /*
+		/*
         EditText editText = (EditText) findViewById(R.id.search);
         editText.setOnEditorActionListener(new OnEditorActionListener() {
 			@Override
@@ -121,33 +152,33 @@ public class DemoActivity extends Activity {
 					KeyEvent event) {
                 if (keyCode == EditorInfo.IME_ACTION_SEARCH ||
                 	(event.getAction() == KeyEvent.ACTION_DOWN && event.getKeyCode() == KeyEvent.KEYCODE_ENTER)) {
-                	
+
                 	return true;
                 }
                 return false;
 			}
         });
-        */
-    }
+		 */
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.options_menu, menu);
-        
-        final MenuItem menuItem = menu.findItem(R.id.action_search);
-        SearchView searchView = (SearchView) menu.findItem(R.id.action_search)
-        		.getActionView();
-        searchView.setOnQueryTextListener(new OnQueryTextListener() {
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.options_menu, menu);
+
+		final MenuItem menuItem = menu.findItem(R.id.action_search);
+		SearchView searchView = (SearchView) menu.findItem(R.id.action_search)
+				.getActionView();
+		searchView.setOnQueryTextListener(new OnQueryTextListener() {
 			@Override
 			public boolean onQueryTextSubmit(String query) {
-				// System.out.println(query);
 				menuItem.collapseActionView();
+		        mViewPager.setCurrentItem(1);
 
-            	new AsyncTask<String, Void, List<String>>() {
+				new AsyncTask<String, Void, List<String>>() {
 					@Override
 					protected List<String> doInBackground(String... params) {
-	                	try {
+						try {
 							return service.get().get(params[0], 5000);
 						} catch (InterruptedException e) {
 							return Lists.newArrayList();
@@ -159,15 +190,15 @@ public class DemoActivity extends Activity {
 							return Lists.newArrayList();
 						}
 					}
-					
+
 					@Override
-				    protected void onPostExecute(List<String> result) {
-						mDisplay.setText("");
+					protected void onPostExecute(List<String> result) {
+						searchFragment.setText("");
 						for (String entry : result) {
-							mDisplay.append(entry);
+							searchFragment.appendText(entry);
 						}
-				    }
-            	}.execute(query);
+					}
+				}.execute(query);
 
 				return false;
 			}
@@ -176,47 +207,129 @@ public class DemoActivity extends Activity {
 			public boolean onQueryTextChange(String newText) {
 				return false;
 			}
-        });
-        
-        return true;
-    }
+		});
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch(item.getItemId()) {
-        	case R.id.action_search:
-        		return true;
-            case R.id.options_debug:
-            	mDisplay.setText(service.get().toString());
-                return true;
-            case R.id.options_exit:
-                finish();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
+		return true;
+	}
 
-    @Override
-    protected void onDestroy() {
-        unregisterReceiver(mHandleMessageReceiver);
-        unbindService(connection);
-        super.onDestroy();
-    }
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch(item.getItemId()) {
+		case R.id.action_search:
+			return true;
+		case R.id.options_debug:
+			debugFragment.setText(service.get().toString());
+			return true;
+		case R.id.options_exit:
+			finish();
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
 
-    private void checkNotNull(Object reference, String name) {
-        if (reference == null) {
-            throw new NullPointerException(
-                    getString(R.string.error_config, name));
-        }
-    }
+	@Override
+	protected void onDestroy() {
+		unregisterReceiver(mHandleMessageReceiver);
+		unbindService(connection);
+		super.onDestroy();
+	}
 
-    private final BroadcastReceiver mHandleMessageReceiver =
-            new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            // String newMessage = intent.getExtras().getString(EXTRA_MESSAGE);
-            // mDisplay.append(newMessage + "\n");
-        }
-    };
+	private void checkNotNull(Object reference, String name) {
+		if (reference == null) {
+			throw new NullPointerException(
+					getString(R.string.error_config, name));
+		}
+	}
+
+	private final BroadcastReceiver mHandleMessageReceiver =
+			new BroadcastReceiver() {
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			// String newMessage = intent.getExtras().getString(EXTRA_MESSAGE);
+			// mDisplay.append(newMessage + "\n");
+		}
+	};
+
+
+	// Since this is an object collection, use a FragmentStatePagerAdapter,
+	// and NOT a FragmentPagerAdapter.
+	public class DemoCollectionPagerAdapter extends FragmentStatePagerAdapter {
+		private final Fragment fragments[];
+		
+		public DemoCollectionPagerAdapter(FragmentManager fm, Fragment fragments[]) {
+			super(fm);
+			this.fragments = fragments;
+		}
+
+		@Override
+		public Fragment getItem(int i) {
+			return fragments[i];
+		}
+
+		@Override
+		public int getCount() {
+			return 3;
+		}
+
+		@Override
+		public CharSequence getPageTitle(int position) {
+			return "OBJECT " + (position + 1);
+		}
+	}
+
+	// Instances of this class are fragments representing a single
+	// object in our collection.
+	public static class DebugFragment extends Fragment {
+		private TextView textView;
+
+		public void setText(String text) {
+			textView.setText(text);
+		}
+		
+		@Override
+		public View onCreateView(LayoutInflater inflater,
+				ViewGroup container, Bundle savedInstanceState) {
+			View rootView = inflater.inflate(
+					R.layout.debug_fragment, container, false);
+			textView = ((TextView) rootView.findViewById(
+					R.id.debug_log));
+			return rootView;
+		}
+	}
+	
+	public static class SearchFragment extends Fragment {
+		TextView mDisplay;
+		
+		public void setText(String text) {
+			mDisplay.setText(text);
+		}
+
+		public void appendText(String text) {
+			mDisplay.append(text);
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater,
+				ViewGroup container, Bundle savedInstanceState) {
+			View rootView = inflater.inflate(
+					R.layout.search_fragment, container, false);
+			
+			mDisplay = (TextView) rootView.findViewById(R.id.serp);
+
+			return rootView;
+		}
+	}
+	
+	public static class CreateFragment extends Fragment {
+
+		@Override
+		public View onCreateView(LayoutInflater inflater,
+				ViewGroup container, Bundle savedInstanceState) {
+			View rootView = inflater.inflate(
+					R.layout.create_fragment, container, false);
+			
+			return rootView;
+		}
+	}
 }
