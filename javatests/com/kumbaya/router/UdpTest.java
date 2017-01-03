06b2a194e8970fba100d900278ff8893a65d99fc
Sam Goto <samuelgoto@gmail.com>
2017-01-03 13:38:06 -0800

    Starting to break the Message implementation into Message-specific subclasses

--- javatests/com/kumbaya/router/UdpTest.java
+++ javatests/com/kumbaya/router/UdpTest.java
@@ -10,17 +10,22 @@ import org.junit.Test;
 
 public class UdpTest extends TestCase {
   private class UdpServer implements Runnable {
+    private final int port;
+    
+    UdpServer(int port) {
+      this.port = port;
+    }
 
     @Override
     public void run() {
       try {
-        DatagramSocket serverSocket = new DatagramSocket(9876);
+        DatagramSocket serverSocket = new DatagramSocket(port);
         byte[] receiveData = new byte[1024];
         DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
         serverSocket.receive(receivePacket);
         Message message = Message.deserialize(receivePacket);
-        String capitalizedSentence = message.getMessage().toUpperCase();
-        Message response = new Message(capitalizedSentence, message.getIPAddress(), message.getPort());
+        String capitalizedSentence = new String(message.getData()).toUpperCase();
+        Message response = new Message(capitalizedSentence.getBytes(), message.getIPAddress(), message.getPort());
         serverSocket.send(response.serialize());
         serverSocket.close();
       } catch (SocketException e) {
@@ -30,21 +35,18 @@ public class UdpTest extends TestCase {
   }
   
   private static class Message {
-    private final String message;
+    private final byte[] data;
     private final InetAddress IPAddress;
     private final int port;
     
-    Message(String message, InetAddress IPAddress, int port) {
-      this.message = message;
+    Message(byte[] data, InetAddress IPAddress, int port) {
+      this.data = data;
       this.IPAddress = IPAddress;
       this.port = port;
     }
     
     DatagramPacket serialize() {
-      byte[] sendData = new byte[1024];
-      String sentence = message;
-      sendData = sentence.getBytes();
-      DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+      DatagramPacket sendPacket = new DatagramPacket(data, data.length, IPAddress, port);
       return sendPacket;
     }
     
@@ -52,11 +54,11 @@ public class UdpTest extends TestCase {
       String modifiedSentence = new String(receivePacket.getData(), 0, receivePacket.getLength());
       InetAddress IPAddress = receivePacket.getAddress();
       int port = receivePacket.getPort();
-      return new Message(modifiedSentence, IPAddress, port);
+      return new Message(modifiedSentence.getBytes(), IPAddress, port);
     }
     
-    String getMessage() {
-      return message;
+    byte[] getData() {
+      return data;
     }
     
     InetAddress getIPAddress() {
@@ -69,25 +71,34 @@ public class UdpTest extends TestCase {
   }
 
   private class UdpClient {    
-    public String send(String inFromUser) throws IOException {
+    private final InetAddress IPAddress;
+    private final int port;
+    
+    UdpClient(InetAddress IPAddress, int port) {
+      this.IPAddress = IPAddress;
+      this.port = port;
+    }
+    
+    
+    public byte[] send(byte[] inFromUser) throws IOException {
       DatagramSocket clientSocket = new DatagramSocket();
-      clientSocket.send(new Message(inFromUser, InetAddress.getByName("localhost"), 9876).serialize());
+      clientSocket.send(new Message(inFromUser, IPAddress, port).serialize());
       byte[] receiveData = new byte[1024];
       DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
       clientSocket.receive(receivePacket);
       Message received = Message.deserialize(receivePacket);
       clientSocket.close();
-      return received.getMessage();
+      return received.getData();
     }
   }
 
   @Test
   public void testServer() throws IOException {
-    Thread server = new Thread(new UdpServer());
+    Thread server = new Thread(new UdpServer(9876));
     server.start();
 
-    UdpClient client = new UdpClient();
-    String result = client.send("hello world");
-    assertEquals("HELLO WORLD", result);
+    UdpClient client = new UdpClient(InetAddress.getByName("localhost"), 9876);
+    byte[] result = client.send("hello world".getBytes());
+    assertEquals("HELLO WORLD", new String(result));
   }  
 }
