cdc3f3880583d8ebd9794bde242682397c0ce22e
Sam Goto <samuelgoto@gmail.com>
2017-01-03 14:32:39 -0800

    breaks the Packet into two separate subclasses for Data packets and Interest packets

--- javatests/com/kumbaya/router/UdpTest.java
+++ javatests/com/kumbaya/router/UdpTest.java
@@ -1,5 +1,7 @@
 package com.kumbaya.router;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
@@ -24,9 +26,20 @@ public class UdpTest extends TestCase {
         DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
         serverSocket.receive(receivePacket);
         Packet message = Packet.of(receivePacket);
-        String capitalizedSentence = new String(message.getData()).toUpperCase();
-        Packet response = new Packet(capitalizedSentence.getBytes(), message.getIPAddress(), message.getPort());
-        serverSocket.send(response.datagram());
+        
+        if (message.getType() == Packet.Type.INTEREST) {
+          InterestPacket interest = (InterestPacket) message;
+          String capitalizedSentence = new String(interest.getName()).toUpperCase();
+          DataPacket response = new DataPacket(
+              capitalizedSentence,
+              capitalizedSentence,
+              message.getIPAddress(), 
+              message.getPort());
+          serverSocket.send(response.datagram());
+        } else if (message.getType() == Packet.Type.DATA) {
+          // TODO(goto): implement this;
+        }
+        
         serverSocket.close();
       } catch (SocketException e) {
       } catch (IOException e) {
@@ -34,27 +47,51 @@ public class UdpTest extends TestCase {
     }
   }
   
-  private static class Packet {
+  private abstract static class Packet {
+    enum Type {
+      INTEREST, DATA
+    }
+    private final Type type;
     private final byte[] data;
     private final InetAddress IPAddress;
     private final int port;
     
-    Packet(byte[] data, InetAddress IPAddress, int port) {
+    Packet(Type type, byte[] data, InetAddress IPAddress, int port) {
+      this.type = type;
       this.data = data;
       this.IPAddress = IPAddress;
       this.port = port;
     }
     
-    DatagramPacket datagram() {
-      DatagramPacket sendPacket = new DatagramPacket(data, data.length, IPAddress, port);
+    DatagramPacket datagram() throws IOException {
+      ByteArrayOutputStream stream = new ByteArrayOutputStream();
+      stream.write(type == Type.INTEREST ? 0 : 1);
+      stream.write(data);
+      DatagramPacket sendPacket = new DatagramPacket(stream.toByteArray(), stream.size(), IPAddress, port);
       return sendPacket;
     }
     
     static Packet of(DatagramPacket receivePacket) {
-      String modifiedSentence = new String(receivePacket.getData(), 0, receivePacket.getLength());
+      // The first byte is reserved for the type of the packet.
+      ByteArrayInputStream stream = new ByteArrayInputStream(
+          receivePacket.getData(), 0, receivePacket.getLength());
+      int type = stream.read();
+      
+      
       InetAddress IPAddress = receivePacket.getAddress();
       int port = receivePacket.getPort();
-      return new Packet(modifiedSentence.getBytes(), IPAddress, port);
+
+      if (type == 0) {
+        // interest packet
+        return InterestPacket.of(stream, IPAddress, port);
+      } else {
+        // data packet
+        return DataPacket.of(stream, IPAddress, port);
+      }
+    }
+    
+    Type getType() {
+      return type;
     }
     
     byte[] getData() {
@@ -69,6 +106,54 @@ public class UdpTest extends TestCase {
       return port;
     }
   }
+  
+  private static class InterestPacket extends Packet {
+    private final String name;
+    
+    InterestPacket(String name, InetAddress IPAddress, int port) {
+      super(Type.INTEREST, name.getBytes(), IPAddress, port);
+      
+      this.name = name;
+    }
+    
+    String getName() {
+      return name;
+    }
+    
+    static InterestPacket of(ByteArrayInputStream stream, InetAddress IPAddress, int port) {
+      // From the second byte forward, you get the type-specific payload.
+      int n = stream.available();
+      byte[] bytes = new byte[n];
+      stream.read(bytes, 0, n);
+      String name = new String(bytes, 0, bytes.length);
+      
+      return new InterestPacket(name, IPAddress, port);      
+    }
+  }
+  
+  private static class DataPacket extends Packet {
+    private final String name;
+    private final String content;
+    
+    DataPacket(String name, String content, InetAddress IPAddress, int port) {
+      super(Type.DATA, name.getBytes(), IPAddress, port);
+      
+      this.name = name;
+      this.content = content;
+    }    
+
+    static DataPacket of(ByteArrayInputStream stream, InetAddress IPAddress, int port) {
+      // From the second byte forward, you get the type-specific payload.
+      int n = stream.available();
+      byte[] bytes = new byte[n];
+      stream.read(bytes, 0, n);
+      String name = new String(bytes, 0, bytes.length);
+      
+      String content = "fake";
+      
+      return new DataPacket(name, content, IPAddress, port);      
+    }
+  }
 
   private class UdpClient {    
     private final InetAddress IPAddress;
@@ -79,9 +164,9 @@ public class UdpTest extends TestCase {
       this.port = port;
     }
     
-    public byte[] send(byte[] inFromUser) throws IOException {
+    public byte[] send(String name) throws IOException {
       DatagramSocket clientSocket = new DatagramSocket();
-      clientSocket.send(new Packet(inFromUser, IPAddress, port).datagram());
+      clientSocket.send(new InterestPacket(name, IPAddress, port).datagram());
       byte[] receiveData = new byte[1024];
       DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
       clientSocket.receive(receivePacket);
@@ -97,7 +182,7 @@ public class UdpTest extends TestCase {
     server.start();
 
     UdpClient client = new UdpClient(InetAddress.getByName("localhost"), 9876);
-    byte[] result = client.send("hello world".getBytes());
+    byte[] result = client.send("hello world");
     assertEquals("HELLO WORLD", new String(result));
   }  
 }
