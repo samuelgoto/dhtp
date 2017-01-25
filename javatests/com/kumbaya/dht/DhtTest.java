3b5922e0eaef0e2c0456b33e583900edd58f3ad0
Sam Goto <samuelgoto@gmail.com>
2017-01-25 11:06:59 -0800

    adds a very basic simple test

--- javatests/com/kumbaya/dht/DhtTest.java
+++ javatests/com/kumbaya/dht/DhtTest.java
@@ -1,175 +1,62 @@
-/**
- * Copyright 2010, 2011 Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.kumbaya.dht;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-import com.kumbaya.dht.Dht;
-import com.kumbaya.dht.Keys;
-
-import org.junit.Before;
+import com.google.inject.AbstractModule;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 import org.junit.Test;
-import org.limewire.io.SimpleNetworkInstanceUtils;
 import org.limewire.mojito.Context;
-import org.limewire.mojito.EntityKey;
-import org.limewire.mojito.MojitoDHT;
 import org.limewire.mojito.MojitoFactory;
 import org.limewire.mojito.db.DHTValueEntity;
-import org.limewire.mojito.db.DHTValueType;
-import org.limewire.mojito.db.impl.DHTValueImpl;
-import org.limewire.mojito.result.FindValueResult;
-import org.limewire.mojito.result.StoreResult;
-import org.limewire.mojito.routing.Version;
-import org.limewire.mojito.settings.NetworkSettings;
-import org.limewire.mojito.util.ContactUtils;
-
-import java.net.InetSocketAddress;
 
 public class DhtTest {
-	@Before
-	public void setUp() {
-		NetworkSettings.LOCAL_IS_PRIVATE.setValue(false);
-		NetworkSettings.FILTER_CLASS_C.setValue(false);
-		ContactUtils.setNetworkInstanceUtils(new SimpleNetworkInstanceUtils(false));
-	}
 
-	@Test
-	public void testDHT() throws Exception {
-		MojitoDHT dht = MojitoFactory.createDHT("bootstrap");
-		dht.bind(new InetSocketAddress("localhost", 8080));
-		dht.start();
 
-		MojitoDHT node = MojitoFactory.createDHT("node");
-		node.bind(new InetSocketAddress("localhost", 8081));
-		node.start();
-		node.bootstrap(new InetSocketAddress("localhost", 8080)).get();
-		assertTrue(node.isBootstrapped());
+  @Test
+  public void testBasic() throws Exception {
+    Dht bootstrap = createDht(8080);
 
-		DHTValueImpl value = new DHTValueImpl(
-				DHTValueType.TEXT, Version.ZERO, "hello world".getBytes());
+    Dht node1 = createDht(8081);
+    Dht node2 = createDht(8082);
 
-		node.put(Keys.of("key"), value).get();
+    node1.bootstrap("localhost", 8080).get(1, TimeUnit.MINUTES);
+    assertTrue(node1.isBootstraped());
 
-		FindValueResult result = node.get(EntityKey.createEntityKey(
-				Keys.of("key"), DHTValueType.TEXT)).get();
+    node2.bootstrap("localhost", 8080).get(1, TimeUnit.MINUTES);
+    assertTrue(node2.isBootstraped());
 
-		assertTrue(result.isSuccess());
-		assertEquals(1, result.getEntities().size());
-		assertEquals("hello world",
-				new String(result.getEntities().iterator().next().getValue().getValue()));
+    // Inserts on node 1.
+    node1.put("foo", "bar");
 
-		node.close();
-		dht.close();
-	}
+    // Fetches from node 2.
+    List<DHTValueEntity> result = node2.get("foo", 200);
 
-	@Test
-	public void testStorables() throws Exception {
-		Context dht = (Context) MojitoFactory.createDHT("bootstrap");
-		dht.getStorableModelManager().addStorableModel(
-				DHTValueType.TEXT, new Dht.Model());
-		dht.bind(new InetSocketAddress("localhost", 8080));
-		dht.start();
+    assertEquals(1, result.size());
+    assertEquals("bar", Values.of(result.get(0)));
 
-		Context node = (Context) MojitoFactory.createDHT("node");
-		node.getStorableModelManager().addStorableModel(
-				DHTValueType.TEXT, new Dht.Model());
-		node.bind(new InetSocketAddress("localhost", 8081));
-		node.start();
-		node.bootstrap(new InetSocketAddress("localhost", 8080)).get();
-		assertTrue(node.isBootstrapped());
+    node1.stop();
+    bootstrap.stop();
+  }
 
-		DHTValueImpl value = new DHTValueImpl(
-				DHTValueType.TEXT, Version.ZERO, "hello world".getBytes());
+  private Dht createDht(int port) throws NumberFormatException, IOException {
+    // Makes sure each instance of the Dht has its own set of classes,
+    // rather than sharing instances of @Singletons for example.
+    Injector injector = Guice.createInjector(new DhtModule(), new AbstractModule() {
+      @Override
+      protected void configure() {
+        bind(Context.class).toInstance((Context) MojitoFactory.createDHT("localhost"));
+      }
+    });
 
-		node.put(Keys.of("key"), value).get();
+    Dht dht = injector.getInstance(Dht.class);
 
-		FindValueResult result = node.get(EntityKey.createEntityKey(
-				Keys.of("key"), DHTValueType.TEXT)).get();
+    dht.start("localhost", port, port);
 
-		assertTrue(result.isSuccess());
-		assertEquals(1, result.getEntities().size());
-		assertEquals("hello world",
-				new String(result.getEntities().iterator().next().getValue().getValue()));
-
-		node.close();
-		dht.close();
-	}
-
-	@Test
-	public void testSettingTheBootstrapNodeAsBootStrapped() throws Exception {
-		Context dht = (Context) MojitoFactory.createDHT("bootstrap");
-		dht.bind(new InetSocketAddress("localhost", 8080));
-		dht.setBootstrapped(true);
-		dht.start();
-
-		MojitoDHT node = MojitoFactory.createDHT("node");
-		node.bind(new InetSocketAddress("localhost", 8081));
-		node.start();
-		node.bootstrap(new InetSocketAddress("localhost", 8080)).get();
-		assertTrue(node.isBootstrapped());
-
-		DHTValueImpl value = new DHTValueImpl(
-				DHTValueType.TEXT, Version.ZERO, "hello world".getBytes());
-
-		node.put(Keys.of("key"), value).get();
-
-		FindValueResult result = dht.get(EntityKey.createEntityKey(
-				Keys.of("key"), DHTValueType.TEXT)).get();
-
-		assertTrue(result.isSuccess());
-		assertEquals(1, result.getEntities().size());
-		assertEquals("hello world",
-				new String(result.getEntities().iterator().next().getValue().getValue()));
-
-		node.close();
-		dht.close();
-	}
-
-	@Test
-	public void testCreatorAddressIsCorrect() throws Exception {
-		Context root = (Context) MojitoFactory.createDHT("bootstrap");
-		root.bind(new InetSocketAddress("localhost", 8081));
-		root.start();
-
-		Context dht = (Context) MojitoFactory.createDHT("dht");
-		dht.bind(new InetSocketAddress("localhost", 8080));
-		dht.start();
-		dht.bootstrap(new InetSocketAddress("localhost", 8081)).get();
-		assertTrue(dht.isBootstrapped());
-
-		DHTValueImpl value = new DHTValueImpl(
-				DHTValueType.TEXT, Version.ZERO, "hello world".getBytes());
-
-		StoreResult store = dht.put(Keys.of("key"), value).get();
-
-		assertEquals(2, store.getLocations().size());
-
-		FindValueResult result = dht.get(EntityKey.createEntityKey(
-				Keys.of("key"), DHTValueType.TEXT)).get();
-
-		assertTrue(result.isSuccess());
-		assertEquals(1, result.getEntities().size());
-		DHTValueEntity entity = result.getEntities().iterator().next();
-		assertEquals(InetSocketAddress.createUnresolved("localhost", 8080),
-				entity.getCreator().getContactAddress());
-		assertEquals(InetSocketAddress.createUnresolved("localhost", 8081),
-				entity.getSender().getContactAddress());
-
-		root.close();
-		dht.close();
-	}
+    return dht;
+  }
 }
---
+++ javatests/com/kumbaya/dht/MojitoDHTTest.java
@@ -0,0 +1,167 @@
+/**
+ * Copyright 2010, 2011 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.kumbaya.dht;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.net.InetSocketAddress;
+import org.junit.Before;
+import org.junit.Test;
+import org.limewire.io.SimpleNetworkInstanceUtils;
+import org.limewire.mojito.Context;
+import org.limewire.mojito.EntityKey;
+import org.limewire.mojito.MojitoDHT;
+import org.limewire.mojito.MojitoFactory;
+import org.limewire.mojito.db.DHTValueEntity;
+import org.limewire.mojito.db.DHTValueType;
+import org.limewire.mojito.db.impl.DHTValueImpl;
+import org.limewire.mojito.result.FindValueResult;
+import org.limewire.mojito.result.StoreResult;
+import org.limewire.mojito.routing.Version;
+import org.limewire.mojito.settings.NetworkSettings;
+import org.limewire.mojito.util.ContactUtils;
+
+public class MojitoDHTTest {
+  @Before
+  public void setUp() {
+    NetworkSettings.LOCAL_IS_PRIVATE.setValue(false);
+    NetworkSettings.FILTER_CLASS_C.setValue(false);
+    ContactUtils.setNetworkInstanceUtils(new SimpleNetworkInstanceUtils(false));
+  }
+
+  @Test
+  public void testDHT() throws Exception {
+    MojitoDHT dht = MojitoFactory.createDHT("bootstrap");
+    dht.bind(new InetSocketAddress("localhost", 8080));
+    dht.start();
+
+    MojitoDHT node = MojitoFactory.createDHT("node");
+    node.bind(new InetSocketAddress("localhost", 8081));
+    node.start();
+    node.bootstrap(new InetSocketAddress("localhost", 8080)).get();
+    assertTrue(node.isBootstrapped());
+
+    DHTValueImpl value =
+        new DHTValueImpl(DHTValueType.TEXT, Version.ZERO, "hello world".getBytes());
+
+    node.put(Keys.of("key"), value).get();
+
+    FindValueResult result =
+        node.get(EntityKey.createEntityKey(Keys.of("key"), DHTValueType.TEXT)).get();
+
+    assertTrue(result.isSuccess());
+    assertEquals(1, result.getEntities().size());
+    assertEquals("hello world",
+        new String(result.getEntities().iterator().next().getValue().getValue()));
+
+    node.close();
+    dht.close();
+  }
+
+  @Test
+  public void testStorables() throws Exception {
+    Context dht = (Context) MojitoFactory.createDHT("bootstrap");
+    dht.getStorableModelManager().addStorableModel(DHTValueType.TEXT, new Dht.Model());
+    dht.bind(new InetSocketAddress("localhost", 8080));
+    dht.start();
+
+    Context node = (Context) MojitoFactory.createDHT("node");
+    node.getStorableModelManager().addStorableModel(DHTValueType.TEXT, new Dht.Model());
+    node.bind(new InetSocketAddress("localhost", 8081));
+    node.start();
+    node.bootstrap(new InetSocketAddress("localhost", 8080)).get();
+    assertTrue(node.isBootstrapped());
+
+    DHTValueImpl value =
+        new DHTValueImpl(DHTValueType.TEXT, Version.ZERO, "hello world".getBytes());
+
+    node.put(Keys.of("key"), value).get();
+
+    FindValueResult result =
+        node.get(EntityKey.createEntityKey(Keys.of("key"), DHTValueType.TEXT)).get();
+
+    assertTrue(result.isSuccess());
+    assertEquals(1, result.getEntities().size());
+    assertEquals("hello world",
+        new String(result.getEntities().iterator().next().getValue().getValue()));
+
+    node.close();
+    dht.close();
+  }
+
+  @Test
+  public void testSettingTheBootstrapNodeAsBootStrapped() throws Exception {
+    Context dht = (Context) MojitoFactory.createDHT("bootstrap");
+    dht.bind(new InetSocketAddress("localhost", 8080));
+    dht.setBootstrapped(true);
+    dht.start();
+
+    MojitoDHT node = MojitoFactory.createDHT("node");
+    node.bind(new InetSocketAddress("localhost", 8081));
+    node.start();
+    node.bootstrap(new InetSocketAddress("localhost", 8080)).get();
+    assertTrue(node.isBootstrapped());
+
+    DHTValueImpl value =
+        new DHTValueImpl(DHTValueType.TEXT, Version.ZERO, "hello world".getBytes());
+
+    node.put(Keys.of("key"), value).get();
+
+    FindValueResult result =
+        dht.get(EntityKey.createEntityKey(Keys.of("key"), DHTValueType.TEXT)).get();
+
+    assertTrue(result.isSuccess());
+    assertEquals(1, result.getEntities().size());
+    assertEquals("hello world",
+        new String(result.getEntities().iterator().next().getValue().getValue()));
+
+    node.close();
+    dht.close();
+  }
+
+  @Test
+  public void testCreatorAddressIsCorrect() throws Exception {
+    Context root = (Context) MojitoFactory.createDHT("bootstrap");
+    root.bind(new InetSocketAddress("localhost", 8081));
+    root.start();
+
+    Context dht = (Context) MojitoFactory.createDHT("dht");
+    dht.bind(new InetSocketAddress("localhost", 8080));
+    dht.start();
+    dht.bootstrap(new InetSocketAddress("localhost", 8081)).get();
+    assertTrue(dht.isBootstrapped());
+
+    DHTValueImpl value =
+        new DHTValueImpl(DHTValueType.TEXT, Version.ZERO, "hello world".getBytes());
+
+    StoreResult store = dht.put(Keys.of("key"), value).get();
+
+    assertEquals(2, store.getLocations().size());
+
+    FindValueResult result =
+        dht.get(EntityKey.createEntityKey(Keys.of("key"), DHTValueType.TEXT)).get();
+
+    assertTrue(result.isSuccess());
+    assertEquals(1, result.getEntities().size());
+    DHTValueEntity entity = result.getEntities().iterator().next();
+    assertEquals(InetSocketAddress.createUnresolved("localhost", 8080),
+        entity.getCreator().getContactAddress());
+    assertEquals(InetSocketAddress.createUnresolved("localhost", 8081),
+        entity.getSender().getContactAddress());
+
+    root.close();
+    dht.close();
+  }
+}
